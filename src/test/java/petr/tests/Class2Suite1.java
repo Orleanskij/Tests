206672aed4b5e434852f4d252058f8f3d09d3005
Orleanskij <Orleanskij.p@yandex.ru>
2020-09-29 13:47:36 +0300

    yes

--- src/test/java/petr/tests/Class2Suite1.java
+++ src/test/java/petr/tests/Class2Suite1.java
@@ -1,15 +1,17 @@
 package petr.tests;
 
 
+import com.ctc.wstx.exc.WstxOutputException;
 import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 
-@Listeners(petr.tests.Hi.class)
+//@Listeners(petr.tests.Hi.class)
 public class Class2Suite1 {
-
+    @BeforeTest()
     @Test(enabled = false)
     @Parameters({"x","y"})
     public void test1(String x, String y) {
